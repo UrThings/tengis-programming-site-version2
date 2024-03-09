06607619e5d8493239756c3743b90b59326a9951
UrThings <160115501+UrThings@users.noreply.github.com>
2024-03-09 15:48:42 +0800

    Add files via upload

---
+++ hiceeliin-medeelel.js
@@ -0,0 +1,53 @@
+export const hiceeluud=[
+  {
+    name: '1р хичээл',
+    title: 'C++ Гэж юу вэ? (Эхний хичээл)',
+    image: "L-1.png",
+    number: '1'
+  },{
+    name: '2р хичээл',
+    title: 'Хөгжүүлэх орчин',
+    image: "L-2.png",
+    number: '2'
+  },{
+    name: '2р хичээл',
+    title: 'Анхны программ',
+    image: "L-3.png",
+    number: '3'
+  },{
+    name: '2р хичээл',
+    title: '2 Тооны нийлбэр олох',
+    image: "L-4.png",
+    number: '4'
+  },{
+    name: '2р хичээл',
+    title: 'Толгой файл',
+    image: "L-5.png",
+    number: '5'
+  },{
+    name: '2р хичээл',
+    title: 'If нөхцөл шалгах үйлдэл',
+    image: "L-6.png",
+    number: '6'
+  },{
+    name: '2р хичээл',
+    title: 'While нөхцөлт давталт гэж юу вэ?',
+    image: "L-7.png",
+    number: '7'
+  },{
+    name: '2р хичээл',
+    title: 'For давталт',
+    image: "L-8.png",
+    number: '8'
+  },{
+    name: '2р хичээл',
+    title: 'Хүндэвтэр бодлого бодож өөрийгөө сорих',
+    image: "L-9.png",
+    number: '9'
+  },{
+    name: '2р хичээл',
+    title: 'Array массивын тухай',
+    image: "L-10.png",
+    number: '10'
+  }
+];
---
+++ tusuliin-medeelel.js
@@ -0,0 +1,29 @@
+
+export const tusuluud = [{
+  name: '1р төсөл',
+  title: 'Дэлгэрэнгүй if нөхцөл шалгах үйлдэл - жижиг програм',
+  image: "T-1.png",
+  number: '101'
+},{
+  name: '2р төсөл',
+  title: 'Жижиг чатын програм хийх',
+  image: "T-2.png",
+  number: '102'
+},{
+  name: '3р төсөл',
+  title: 'Онгоц бууддаг тоглоом',
+  image: "T-3.png",
+  number: '103'
+},{
+  name: '4р төсөл',
+  title: 'Толь бичгийн програм зохиох',
+  image: "T-4.png",
+  number: '104'
+},{
+  name: '5р төсөл',
+  title: 'ATM програм хийцгээе',
+  image: "T-5.png",
+  number: '105'
+}];
+
+
